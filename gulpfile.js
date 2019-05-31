a39eb2a1c2ef3ecb4b3cbe10aa55f969fd77ec70
Don Liggett <donliggett@gmail.com>
2019-05-31 16:36:56 -0400

    add gulp-clean-sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@
 // Load plugins
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const cleanCSS = require('gulp-clean-css');
 const rename = require("gulp-rename");
 const plumber = require("gulp-plumber");
 
+// Compile CSS
 function css() {
   return gulp
     .src("./handdrawn.scss")
@@ -13,9 +15,13 @@ function css() {
     .pipe(sass({ outputStyle: "expanded" }))
     .pipe(gulp.dest("./assets/css/"))
     .pipe(rename({ suffix: ".min" }))
+    .pipe(cleanCSS({debug: true, compatibility: 'ie8'}, (details) => {
+      console.log(`Compressed: ${details.name} ${(details.stats.originalSize * 0.001).toFixed(2)}KB => ${(details.stats.minifiedSize * 0.001).toFixed(2)}KB`);
+    }))
     .pipe(gulp.dest("./assets/css/"));
 }
 
+// Pipe Boostrap JS
 function js() {
   return gulp
     .src("./node_modules/bootstrap/dist/js/bootstrap.min.js")
